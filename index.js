0e7e33c4ccb187a70ee17b1dcfa3f30533813635
Christophe Massolin <christophe.massolin@gmail.com>
2017-06-27 01:52:46 +0200

    Init argparser

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const path      = require('path')
+const ARGParser = require('./lib/arg-parser2')
+
+const ARGParserSingleton = new ARGParser
+
+ARGParserSingleton.checker('boolean', function( parameter, value ) {
+  return typeof value === 'boolean'
+})
+
+ARGParserSingleton.checker('value', function( parameter, value ) {
+  return value !== null && value !== undefined && value !== '' && typeof value !== 'boolean'
+})
+
+ARGParserSingleton.checker('select', function( parameter, value ) {
+  console.log('"select" type is deprecated. Prefer "enum" type.')
+  return ARGParserSingleton._checkers['enum']( parameter, value )
+})
+
+ARGParserSingleton.checker('enum', function( parameter, value ) {
+  if (parameter.values && parameter.values.indexOf(value) === -1) {
+    return false
+  }
+  return true
+})
+
+ARGParserSingleton.checker('file', function( parameter, value ) {
+  let res = false
+  let ext = ''
+
+  if (ARGParserSingleton._checkers['value'](parameter, value)) {
+    ext = path.extname(value)
+    res = ext.length > 1
+  }
+
+  if (parameter.extensions) {
+    res = parameter.extensions.indexOf(ext) !== -1
+  }
+
+  return res
+})
+
+ARGParserSingleton.new = (function() {
+  const parser     = new ARGParser(...arguments)
+  parser._checkers = Object.assign({}, this._checkers)
+  return parser
+}).bind(ARGParserSingleton)
+
+module.exports = ARGParserSingleton
---
+++ test/index.js
@@ -0,0 +1,61 @@
+const ARGParser = require('../index')
+
+const config = {
+  who: {
+    type: 'value',
+    no_key: true,
+    index: 2,
+    defaultValue: 'John',
+    description: 'Set who is talking'
+  },
+
+  message: {
+    type: 'value',
+    defaultValue: 'Hello World',
+    description: 'Set message'
+  },
+
+  status: {
+    type: 'enum',
+    values: [ 'pending', 'draft', 'published' ],
+    defaultValue: 'pending',
+    description: 'Set status'
+  },
+
+  file: {
+    type: 'file',
+    extensions: [ '.txt', '.md' ],
+    description: 'Set filename'
+  },
+
+  verbose: {
+    type: 'boolean',
+    aliases: [ 'v' ],
+    defaultValue: false,
+    description: 'Display logs'
+  }
+}
+
+// const res0 = ARGParser.parse('wk hello')
+// console.log(res0.valid_params)
+
+// const res1 = ARGParser.parse('wk hello Max', config)
+// console.log(res1.valid_params)
+
+const res2 = ARGParser.parse('wk hello --message "Salut tout le monde"', config)
+// console.log(res2.valid_params)
+
+// const res3 = ARGParser.parse('wk hello --status something', config)
+// console.log(res3.valid_params)
+
+// const res4 = ARGParser.parse('wk hello --file hello.txt', config)
+// console.log(res4.valid_params)
+
+const res5 = ARGParser.parse('wk hello -v', config)
+// console.log(res5.valid_params)
+
+console.log(ARGParser.join(res5.valid_params))
+console.log(ARGParser.format(res5, true))
+
+console.log(res5.valid_params)
+console.log(ARGParser.setParameters(res5, res2.params).valid_params)
