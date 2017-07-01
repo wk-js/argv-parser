6ebcff2a9e9f6048564b6600338b47a79eedf25d
Christophe Massolin <christophe.massolin@gmail.com>
2017-07-01 17:40:45 +0200

    Apply type

--- test/command.js
+++ test/command.js
@@ -5,18 +5,38 @@ const ARGParser = require('../index')
 const command = ARGParser
 .command('hello')
 
-.index('who', 1)
-.default('who', 'John')
-.describe('who', 'Who is talking ?')
-
-.string('message')
-.required(['who', 'message'], 'veuillez préciser un message')
-.coerce('message', function(v) {
-  return v === "Salut" ? "Plop" : v
+// .index('who', 1)
+// .default('who', 'John')
+// .describe('who', 'Who is talking ?')
+// ==> Same as
+.option('who', {
+  no_key: true,
+  index: 1,
+  defaultValue: 'John',
+  description: 'Who is talking ?'
 })
 
-.file('package', [ '.json' ])
-.alias('package', 'p')
+// .string('message')
+// .coerce('message', function(v) {
+//   return v === "Salut" ? "Plop" : v
+// })
+// ==> Same as
+.option('message', {
+  as: 'string',
+  coerce(v) {
+    return v === "Salut" ? "Plop" : v
+  }
+})
+
+// .file('package', [ '.json' ])
+// .alias('package', 'p')
+// ==> Same as
+.option('package', {
+  type: 'file',
+  aliases: [ 'p' ]
+})
+
+.required(['who', 'message'], 'veuillez préciser un message')
 
 .help()
 
